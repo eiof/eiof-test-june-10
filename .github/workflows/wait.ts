b82589ea41a46f853c1e516692c23d11c47c6ef4
Matthew Elliott <matt@dbtlabs.com>
2023-05-25 14:58:41 -0700

    test new script

--- .github/workflows/wait.ts
+++ .github/workflows/wait.ts
@@ -13,24 +13,14 @@ const getRuns = async (github, context, input) => {
     branch: input.branch,
   };
 
-  const inProgressOptions = {
-    ...options,
-    status: 'in_progress',
-  };
-
-  const queuedOptions = {
-    ...options,
-    status: 'queued',
-  };
-
   const inProgressRunsPromise = github.paginate(
     github.rest.actions.listWorkflowRunsForRepo,
-    inProgressOptions,
+    { ...options, status: 'in_progress' }
   );
 
   const queuedRunsPromise = github.paginate(
     github.rest.actions.listWorkflowRunsForRepo,
-    queuedOptions,
+    { ...options, status: 'queued' }
   );
 
   const fetchRuns = async () => await Promise.all([
@@ -54,31 +44,36 @@ const getRuns = async (github, context, input) => {
   throw lastError;
 };
 
+const cancelSelf = async (github, context) => {
+  console.log('Canceling this workflow, a newer workflow has started.');
+  
+  const { owner, repo } = context.repo;
+  await github.rest.actions.cancelWorkflowRun({
+    owner,
+    repo,
+    run_id: context.runId,
+  });
+
+  // Wait up to five minutes for cancelation
+  await delay(300 * 100)
+
+  // Fail the workflow if cancelation is taking too long
+  throw new Error("Unexpectedly failed to cancel this workflow.");
+}
+
 const wait = async (github, context, input) => {
   console.log('Checking for other workflows...');
 
   const workflowName = context.workflow;
+  const runId = context.runId;
 
   while (true) {
     const runs = await getRuns(github, context, input);
 
-    const newerRuns = runs
-      .filter((run) => run.name === workflowName && run.id > context.runId);
+    const newerRun = runs.find((run) => run.name === workflowName && run.id > runId);
 
-    if (newerRuns.length) {
-      console.log('Canceling this workflow, a newer workflow has started.');
-
-      const { owner, repo } = context.repo;
-      await github.rest.actions.cancelWorkflowRun({
-        owner,
-        repo,
-        run_id: context.runId,
-      });
-
-      // Wait up to two minutes for cancelation
-      // or fall through loop and try again
-      await delay(120 * 100)
-      continue;
+    if (newerRun) {
+      await cancelSelf(github, context);
     }
 
     const previousRuns = runs
@@ -91,11 +86,9 @@ const wait = async (github, context, input) => {
     }
 
     console.log('Awaiting runs:');
-    for (const run of previousRuns) {
-      console.log(`  ${run.html_url}`);
-    }
+    previousRuns.forEach((run) => console.log(`  ${run.html_url}`));
 
-    // Wait 30 seconds before checking again
+    // Wait before checking again
     await delay(input.delay * 1000);
   }
 };
